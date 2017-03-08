1e705e7dea2b2ff4dd0e0fc3b238f0c2c3b4f972
Andrey <gmtyllo@gmail.com>
2017-03-09 00:48:49 +1000

    Add proxy for api for development

--- config/index.js
+++ config/index.js
@@ -28,6 +28,10 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
+      '/api': {
+        target: 'http://127.0.0.1:8044',
+        changeOrigin: true,
+      }
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
